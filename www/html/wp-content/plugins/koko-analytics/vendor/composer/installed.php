14342181ece3ec94a25c353a83c1144d449cb964
Anil Kulkarni <anil.kulkarni@outlook.com>
2021-03-03 11:38:12 -0800

    Install koko-analytics and updraft

---
+++ www/html/wp-content/plugins/koko-analytics/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '95a93198b55cae060a9970800eeaac1d6c0da5ed',
+    'name' => 'ibericode/koko-analytics',
+  ),
+  'versions' => 
+  array (
+    'ibericode/koko-analytics' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '95a93198b55cae060a9970800eeaac1d6c0da5ed',
+    ),
+  ),
+);
