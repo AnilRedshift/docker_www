4bf3fd119485ceb9cd4da591e9c25b6f4d3f02c5
Anil Kulkarni <anil.kulkarni@outlook.com>
2021-03-08 12:03:03 -0800

    Backup SSL certs

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,6 @@
 echo "Running restic backup"
 restic --verbose=3 backup /var/www/html/wp-content/updraft --exclude="log*"
+restic --verbose=3 backup /etc/ssl/certs/private/terminal.space
 # Purge anything older than a month
 restic --verbose=3 forget --keep-within 1m
  
