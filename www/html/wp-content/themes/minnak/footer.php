1149bb38bb0c2d6ee64a318aaabb85d392371303
Anil Kulkarni <anil.kulkarni@outlook.com>
2021-03-10 14:52:58 -0800

    Change to all-time visitors

--- www/html/wp-content/themes/minnak/footer.php
+++ www/html/wp-content/themes/minnak/footer.php
@@ -10,19 +10,19 @@
  */
 
 ?>
-		<footer id="colophon" class="site-footer">
-		<div id="site-info" class="site-info">
-			<?php minnak_site_copyright(); ?>
-			<span>
-					Visitors:
-					<?php
-    				echo do_shortcode('[koko_analytics_site_counter days=30]');
-					?>
-			</span>
-			<?php minnak_credit(); ?>
-		</div>
-		</footer>
-	</main>
+    <footer id="colophon" class="site-footer">
+    <div id="site-info" class="site-info">
+      <?php minnak_site_copyright(); ?>
+      <span>
+          Visitors:
+          <?php
+            echo do_shortcode('[koko_analytics_site_counter]');
+          ?>
+      </span>
+      <?php minnak_credit(); ?>
+    </div>
+    </footer>
+  </main>
 </div>
 
 <?php wp_footer(); ?>
