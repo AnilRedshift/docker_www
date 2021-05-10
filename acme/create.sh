e79125c3a66e53f702b0b62b315c2cf7a0fd2eb0
Anil Kulkarni <anil.kulkarni@outlook.com>
2021-05-10 12:36:12 -0700

    Fix certificate to get wildcard and to reload properly

--- acme/create.sh
+++ acme/create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/root/.acme.sh/acme.sh --issue --dns dns_gandi_livedns -d terminal.space -d *.terminal.space
+/root/.acme.sh/acme.sh --issue --dns dns_gandi_livedns -d 'terminal.space' -d '*.terminal.space'
--- acme/renew.sh
+++ acme/renew.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-/root/.acme.sh/acme.sh --issue --dns dns_gandi_livedns -d terminal.space -d *.terminal.space
-ssh -i /nginx_ssh_rsa root@nginx_reverse_proxy 'nginx -s reload'
+/root/.acme.sh/acme.sh --issue --dns dns_gandi_livedns -d terminal.space -d '*.terminal.space'
+chmod 600 /nginx_ssh_rsa
+ssh -o "StrictHostKeyChecking no" -i /nginx_ssh_rsa root@nginx_reverse_proxy 'nginx -s reload'
