aef8545040cb7bd2ea3366affa594b683b088f9e
Anil Kulkarni <anil.kulkarni@outlook.com>
2021-06-02 01:17:42 -0700

    Fix muuri errors on Firefox

--- www/html/wp-content/themes/minnak/js/muuri-options.js
+++ www/html/wp-content/themes/minnak/js/muuri-options.js
@@ -1,4 +1,7 @@
-jQuery(document).ready(function( $ ){
+jQuery(document).ready(function ($) {
   /* MASONARY GRID */
+  // https://github.com/haltu/muuri/issues/416
+  Muuri.defaultPacker.destroy();
+  Muuri.defaultPacker = new Muuri.Packer(0);
   var grid = new Muuri('.grid');
 });
